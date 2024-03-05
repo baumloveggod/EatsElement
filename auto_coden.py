7ed4c1584589c11fc08d4991ddb283c24fde7ae4
baumloveggod <82512269+baumloveggod@users.noreply.github.com>
2024-03-05 23:28:06 +0100

    "Update auto_coden.py and http/Views/pages/zutaten.php to reflect changes in output.txt"

--- auto_coden.py
+++ auto_coden.py
@@ -88,7 +88,7 @@ def git_commit_and_push(commit_message):
         print(f"Fehler beim Ausführen von Git-Befehlen: {e}")
 #,'
 # Main script
-write_file_info_to_output(['./http/Views/templates/einheitenFormular.php','./http/Views/pages/einheiten.php', './http/Views/pages/zutaten.php','sql_create'], 'output.txt')
+write_file_info_to_output(['./http/Views/templates/zutatenFormular.php','./http/Views/templates/einheitenFormular.php','./http/Views/pages/einheiten.php', './http/Views/pages/zutaten.php','sql_create'], 'output.txt')
 changed_files = get_git_changes()
 if changed_files:  # Only proceed if there are changed files
     commit_message = generate_commit_message(changed_files)

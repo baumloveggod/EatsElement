7712481716b701282afeb6f15f24085726d883db
baumloveggod <82512269+baumloveggod@users.noreply.github.com>
2024-03-27 11:58:51 +0100

    "Update auto_coden.py to include new functionality for generating code."

--- auto_coden.py
+++ auto_coden.py
@@ -94,7 +94,7 @@ c = ['./http']
 d = ['./http/Views/pages/zutaten.php','./http/Views/templates/']
 e = ['./http/Views/pages/zutaten.php','./http/Views/templates/zutatenFormular.php','./http/Views/templates/formFunctions.js']
 
-f = ['./http/Views/pages/rezepte.php','./http/Views/templates/rezepteFormular.php','./http/Views/templates/rezepte_post.php','./http/Views/pages/zutaten.php','./http/Views/templates/zutatenFormular.php','./http/Views/templates/formFunctions.js','sql']
+f = ['./http/Views/pages/rezepte.php','./http/Views/templates/rezepteFormular.php','./http/Views/templates/rezepte_post.php','./http/Views/pages/zutaten.php','./http/Views/templates/zutatenFormular.php','./http/Views/templates/formFunctions.js','./sql']
 write_file_info_to_output(f, 'output.txt')
 changed_files = get_git_changes()
 if changed_files:  # Only proceed if there are changed files
